971fad06542504a807fe6185ff103c45317e1b05
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-11-29 20:12:11 +0530

    slider

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -6,11 +6,21 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
+import ReactSlider from 'react-slider'
 // import required modules
 import { FreeMode, Pagination } from "swiper";
 const LandingPage = () => {
   return (
     <div>
+      <div className="w-full h-8 flex flex-col justify-start items-center gap-10 bg-red-500">
+        <ReactSlider
+          className="horizontal-slider w-full h-full bg-red-900 p-0"
+          thumbClassName="example-thumb bg-black text-white"
+          trackClassName="example-track bg-red-500"
+          renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+          onChange={console.log}
+        />
+      </div>
       <footer className="bg-[#f2dbd0]">
         <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
           <h1 className="text-2xl text-black">Made with ❤️ for FilBanglore</h1>
